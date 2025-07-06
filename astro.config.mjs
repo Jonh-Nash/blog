588b3df33ab4c4cb13e697e40a48432d31a236f5
justy <vicious8424687@gmail.com>
2025-07-06 13:44:59 +0900

    Astroのベースパスを環境変数に基づいて設定し、リンクを動的に生成するように変更しました。これにより、異なる環境でのデプロイが容易になります。

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://Jonh-Nash.github.io",
-  base: process.env.NODE_ENV === "production" ? "/blog/" : "/",
+  base: "/blog/",
   content: {
     dir: "content",
   },
